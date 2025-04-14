18219fbb6de513dfd5109dbafe204f1ec6903584
Phan Bình Nguyên Lâm <76941117+LLaammTTeerr@users.noreply.github.com>
2025-04-14 21:25:26 +0700

    add smart mode to generator

--- src/generator/gen.cpp
+++ src/generator/gen.cpp
@@ -3,6 +3,10 @@
 
 const int MAX = 1000000000;
 
+int wnextr(int l, int r, int b) {
+	return rnd.wnext(r - l + 1, b) + l;
+}
+
 int main(int argc, char** argv) {
 	registerGen(argc, argv, 1);
 
@@ -28,25 +32,65 @@ int main(int argc, char** argv) {
 	std::vector<int> order(n); std::iota(order.begin(), order.end(), 0);
 	shuffle(order.begin(), order.end());
 	std::vector<std::tuple<int, int, int, int>> edges; edges.reserve(m);
-	for (int i = 1; i < n; i += 1) {
-		int u = order[i];
-		int v = order[parent[i]];
-		int t = rnd.next(0, max_t);
-		int d = rnd.next(1, max_d);
-		if (rnd.next(2)) {
-			std::swap(u, v);
+
+	if (mode == "random") {
+		for (int i = 1; i < n; i += 1) {
+			int u = order[i];
+			int v = order[parent[i]];
+			int t = rnd.next(0, max_t);
+			int d = rnd.next(1, max_d);
+			if (rnd.next(2)) {
+				std::swap(u, v);
+			}
+			edges.emplace_back(u, v, t, d);
 		}
-		edges.emplace_back(u, v, t, d);
+		while ((int) edges.size() < m) {
+			int u = rnd.next(n);
+			int v = rnd.next(n);
+			while (u == v)
+				v = rnd.next(n);
+			int t = rnd.next(0, max_t);
+			int d = rnd.next(1, max_d);
+			edges.emplace_back(u, v, t, d);
+		}
 	}
-	while ((int) edges.size() < m) {
-		int u = rnd.next(n);
-		int v = rnd.next(n);
-		while (u == v)
-			v = rnd.next(n);
-		int t = rnd.next(0, max_t);
-		int d = rnd.next(1, max_d);
-		edges.emplace_back(u, v, t, d);
+
+	if (mode == "smart") {
+		std::vector<std::vector<int>> g(n);
+		for (int i = 1; i < n; i += 1) {
+			int u = order[i];
+			int v = order[parent[i]];
+			g[u].emplace_back(v);
+			g[v].emplace_back(u);
+		}
+		std::vector<int> ans(n); ans[x] = t_min;
+		auto dfs = [&] (auto& dfs, int u, int p) -> void {
+			for (int v : g[u]) if (v ^ p) {
+				int t = wnextr(ans[u], std::max(ans[u], t_max), -bias_t);
+				int d = rnd.next(std::max(1, std::min(max_d, t_max - t))) + 1;
+				edges.emplace_back(u, v, t, d);
+				ans[v] = t + d;
+				dfs(dfs, v, u);
+			}
+		};
+		dfs(dfs, x, x);
+		while ((int) edges.size() < m) {
+			int u = rnd.next(n);
+			int v = rnd.next(n);
+			while (u == v)
+				v = rnd.next(n);
+			if (ans[u] > ans[v]) {
+				int t = rnd.wnext(max_t, -bias_t);
+				int d = rnd.next(1, max_d);
+				edges.emplace_back(u, v, t, d);
+			} else {
+				int t = rnd.wnext(ans[u], ans[v], -bias_t);
+				int d = rnd.next(1, std::min(max_d, std::max(1, ans[v] - ans[u])));
+				edges.emplace_back(u, v, t, d);
+			}
+		}
 	}
+
 	assert((int) edges.size() == m);
 	shuffle(edges.begin(), edges.end());
 	std::sort(edges.begin(), edges.end(),
